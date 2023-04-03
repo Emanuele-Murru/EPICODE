fa9ac59b75182e5608ea16daf6fca37070851c14
Emanuele Murru <emanuelemurru.m98@gmail.com>
2023-04-03 23:14:04 +0200

    Day1 Week2

---
+++ Settimana2/Day1/assets/js/pagina2.js
@@ -0,0 +1,10 @@
+var ilNano = 'criceto';
+var LoSpilungone = 'giraffa';
+var ilPossente = 'leone';
+
+console.log(ilNano);
+console.log(LoSpilungone);
+console.log(ilPossente);
+
+var black_panther = 'pantera';
+document.write('<div class="container">' + black_panther + ' scritto da JavaScript');
---
+++ Settimana2/Day1/assets/js/script.js
@@ -0,0 +1,10 @@
+window.alert('CIAO 1');
+window.alert('CIAO 2');
+
+
+document.getElementById('Titolo').innerHTML = 'Iniziamo la nostra pratica';
+
+document.write('<div class="container"> output scritto da JavaScript</div>');
+
+
+
