8e1be7934eb0a858cc225cdd96d3d75636db6bef
Emanuele Murru <emanuelemurru.m98@gmail.com>
2023-04-07 15:40:13 +0200

    ProgettoSettimana02

--- Settimana2/ProgettoSettimana02/assets/js/script.js
+++ Settimana2/ProgettoSettimana02/assets/js/script.js
@@ -9,20 +9,52 @@ REGOLE
 /* ESERCIZIO 1
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
-const pets = ['dog', 'cat', 'hamster', 'redfish']
+
+// const pets = ['dog', 'cat', 'hamster', 'redfish']
+
+// for(i = 0; i < pets.length; i++){
+//   console.log(pets[i])
+// }
+
 
 
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
 
+// const pets = ['dog', 'cat', 'hamster', 'redfish']
+// for( i = 0; i < pets.length; i++){
+//   pets.sort()
+//   console.log(pets[i]) 
+// }
+
+
+
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
 
+// const pets = ['dog', 'cat', 'hamster', 'redfish']
+// pets.reverse()
+
+// for( i = 0; i < pets.length; i++){
+//   console.log(pets[i])
+// };
+
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
+
+// const pets = ['dog', 'cat', 'hamster', 'redfish']
+// pets.shift()
+// pets.push('dog')
+
+// for(i = 0; i < pets.length; i++){
+//   console.log(pets[i])
+// }
+
+// console.log(pets)
+
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
@@ -58,7 +90,7 @@ cars.push({
   model: "GT",
   color: "black",
   trims: ["turbo", "GT", "sport-series"]
-  
+
 })
 
 /* ESERCIZIO 7
