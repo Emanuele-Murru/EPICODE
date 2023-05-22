bc097d6370d43d54d879fef8f7e2d336f1be77ad
Emanuele <126979214+Emanuele-Murru@users.noreply.github.com>
2023-05-22 21:10:19 +0200

    update

--- Settimana9/Day1/assets/js/script.js
+++ Settimana9/Day1/assets/js/script.js
@@ -1,20 +1,18 @@
 // Creiamo un algoritmo che generi un numero casuale compreso tra 1 e 100 (zero escluso)
-let randomNumber = () => {
-    return Math.floor(Math.random() * 100 + 1);
-};
-console.log(randomNumber());
-let giocatore1 = randomNumber();
-let giocatore2 = randomNumber();
-let dado = randomNumber();
-console.log(giocatore1, giocatore2, dado);
-if (giocatore1 && giocatore2 !== dado) {
-    if (Math.abs(dado - giocatore1) > (Math.abs(dado - giocatore2))) {
-        console.log("Il giocatore 2 vince/ e' andato piu vicino");
-    }
-    else {
-        console.log("Il giocatore 1 vince/ e' andato piu vicino");
-    }
-}
-else {
-    console.log("Pareggio");
-}
+// let randomNumber = ():number => {
+//      return Math.floor(Math.random()* 100 +1)
+// };
+// console.log(randomNumber())
+// let giocatore1 = randomNumber();
+// let giocatore2 = randomNumber();
+// let dado = randomNumber();
+// console.log(giocatore1, giocatore2, dado)
+// if(giocatore1 && giocatore2 !== dado) {
+//     if(Math.abs(dado - giocatore1) > (Math.abs(dado - giocatore2))) {
+//         console.log("Il giocatore 2 vince/ e' andato piu vicino");
+//     }else {
+//         console.log("Il giocatore 1 vince/ e' andato piu vicino")
+//     }
+// } else {console.log("Pareggio");}
+let player1 = document.getElementById('giocatore1').value;
+console.log(player1);
