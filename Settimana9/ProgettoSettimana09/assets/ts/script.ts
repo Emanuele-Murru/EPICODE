191ea8a9ec03d70ffcd531012093dbce84907447
Emanuele <126979214+Emanuele-Murru@users.noreply.github.com>
2023-05-26 15:33:30 +0200

    ProgettoSettimana09

--- Settimana9/ProgettoSettimana09/assets/ts/script.ts
+++ Settimana9/ProgettoSettimana09/assets/ts/script.ts
@@ -49,27 +49,49 @@ const thirdUser = new User();
 // Primo utente
 console.log("Primo utente:");
 console.log("------------------------------------------------");
+
 firstUser.ricarica(50);
 console.log(`Credito residuo: ${firstUser.numero404()} euri`);
+
+console.log("Chiamata:");
 firstUser.chiamata(7);
+console.log(`Credito residuo: ${firstUser.numero404()} euri`);
+
 console.log(`Numero chiamate: ${firstUser.getNumeroChiamate()}`);
 console.log(`Chiamate azzerate: ${firstUser.numeroChiamate}`);
 firstUser.azzeraChiamate();
 console.log(`Numero chiamate: ${firstUser.getNumeroChiamate()}`);
+
 console.log("");
 
 // Secondo utente
 console.log("Secondo utente:");
 console.log("------------------------------------------------");
+
 secondUser.ricarica(150);
 console.log(`Credito residuo: ${secondUser.numero404()} euri`);
+
+console.log("Chiamata:");
 secondUser.chiamata(30);
-secondUser.chiamata(50);
-secondUser.chiamata(18);
-secondUser.chiamata(22);
-secondUser.chiamata(45);
-console.log(`Numero chiamate: ${secondUser.getNumeroChiamate()}`);
 console.log(`Credito residuo: ${secondUser.numero404()} euri`);
+
+console.log("Chiamata:");
+secondUser.chiamata(50);
+console.log(`Credito residuo: ${secondUser.numero404()} euri`);
+
+console.log("Chiamata:");
+secondUser.chiamata(18);
+console.log(`Credito residuo: ${secondUser.numero404()} euri`);
+
+console.log("Chiamata:");
+secondUser.chiamata(22);
+console.log(`Credito residuo: ${secondUser.numero404()} euri`);
+
+console.log("Chiamata:");
+secondUser.chiamata(45);
+console.log(`Credito residuo: ${secondUser.numero404()} euri`);
+
+console.log(`Numero chiamate: ${secondUser.getNumeroChiamate()}`);
 secondUser.azzeraChiamate();
 console.log(`Chiamate azzerate: ${secondUser.numeroChiamate}`);
 console.log("");
@@ -77,15 +99,25 @@ console.log("");
 // Terzo utente
 console.log("Terzo utente:");
 console.log("------------------------------------------------");
+
 thirdUser.ricarica(15);
 console.log(`Credito residuo: ${thirdUser.numero404()} euri`);
+
+console.log("Chiamata");
 thirdUser.chiamata(6);
+console.log(`Credito residuo: ${thirdUser.numero404()} euri`);
+
 console.log(`Numero chiamate: ${thirdUser.getNumeroChiamate()}`);
+
+console.log("Chiamata");
 thirdUser.ricarica(10);
 console.log(`Credito residuo: ${thirdUser.numero404()} euri`);
+
+console.log("Chiamata");
 thirdUser.chiamata(8);
-console.log(`Numero chiamate: ${thirdUser.getNumeroChiamate()}`);
 console.log(`Credito residuo: ${thirdUser.numero404()} euri`);
+
+console.log(`Numero chiamate: ${thirdUser.getNumeroChiamate()}`);
 console.log(`Chiamate azzerate: ${thirdUser.numeroChiamate}`);
 thirdUser.azzeraChiamate();
 console.log(`Numero chiamate: ${thirdUser.getNumeroChiamate()}`);
